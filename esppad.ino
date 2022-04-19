0e58f65aaadaa9361aac87580813f47ac1c4b2f1
PiotrOchal <88327884+PiotrOchal@users.noreply.github.com>
2022-04-19 19:47:26 +0200

    esp

--- esppad.ino
+++ esppad.ino
@@ -1,54 +1,81 @@
+//created by Piotr Ochal
+
+//button ABXY
 const int bA = 32;
 const int bB = 5;
 const int bX = 35;
 const int bY = 17;
-const int gas = 12;
-const int ham = 14;
-const int kier = 27;
+//triggers use potentiometers
+const int Tr = 12;
+const int Tl = 14;
+//joystic  use potentiometers
+const int Jlx = 27;
+
+
+const int Jly = 33;
+const int Jrx = 25;
+const int Jry = 36;
+ 
+
+//variables
 int vA;
 int vB;
 int vX;
 int vY;
-int vgas;
-int vham;
-int vkier;
-void setup() {
+int vTr;
+int vTl;
+int vJlx;
 
+int vJly;
+int vJrx;
+int vJry;
+ 
+void setup() {
   pinMode (bA, INPUT);
   pinMode (bB, INPUT);
   pinMode (bX, INPUT);
   pinMode (bY, INPUT);
-
-
-    
-  
   Serial.begin (9600);
 }
 
 void loop() {
-  vkier=analogRead(kier);
+  //read variables
+  
   vA = digitalRead(bA);
   vX = digitalRead(bX);
   vB = digitalRead(bB);
   vY = digitalRead(bY);
-  vgas =analogRead(gas);
-  vham=analogRead(ham);
- 
-    
-    Serial.print("a");
-    Serial.print(vA);
-    Serial.print("b");
-    Serial.print(vB);
-    Serial.print("x");
-    Serial.print(vX);
-    Serial.print("y");
-    Serial.print(vY);
-    Serial.print("Jlx");
-    Serial.print(vkier+1000);
-    Serial.print("Tl");
-    Serial.print(vham+1000);
-    Serial.print("Tr");
-    Serial.println(vgas+1000);
+  vTr=analogRead(Tr);
+  vTl=analogRead(Tl);
+  vJlx=analogRead(Jlx);
+  
+  vJly=analogRead(Jly);
+  vJrx=analogRead(Jrx);
+  vJry=analogRead(Jry);
+  
+  //write to serial port
+  Serial.print("a");
+  Serial.print(vA);
+  Serial.print("b");
+  Serial.print(vB);
+  Serial.print("x");
+  Serial.print(vX);
+  Serial.print("y");
+  Serial.print(vY);
+  Serial.print("Jlx");
+  Serial.print(vJlx+1000);//add 1000 becuese constant length is easier to read
+  Serial.print("Jly");
+  Serial.print(vJly+1000);
+  Serial.print("Jrx");
+  Serial.print(vJrx+1000);
+  Serial.print("Jry");
+  Serial.print(vJry+1000);
+  
+  
+  Serial.print("Tl");
+  Serial.print(vTl+1000);
+  Serial.print("Tr");
+  Serial.println(vTr+1000);
     
   
 }
